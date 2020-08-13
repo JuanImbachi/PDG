08950e975c3fef432cd6856f504277e91936aa42
Jamf99 <jorge.morales2@correo.icesi.edu.co>
2020-08-13 11:38:57 -0500

    images

--- PDG/frontend/src/components/Home.vue
+++ PDG/frontend/src/components/Home.vue
@@ -1,12 +1,45 @@
 <template>
+<v-container>
+    <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+</v-container>
 </template>
 
 <script>
 export default {
-
-}
+    data () {
+      return {
+        items: [
+          {
+            src: '/PDG/frontend/src/assets/1.png',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+      }
+    },
+  }
 </script>
 
 <style>
+
+.container {
+    margin-top : 4%;
+    
+}
 
 </style>
