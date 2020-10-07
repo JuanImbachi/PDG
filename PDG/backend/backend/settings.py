be84b54d3dc3bfeb90044e0fa20ba1f8e40fa3ae
Juan Manuel Imbachí G <juan.imbachi@correo.icesi.edu.co>
2020-10-06 20:53:14 -0500

    Backend gerenal settings modified

--- PDG/backend/backend/settings.py
+++ PDG/backend/backend/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@b#cp-%--yfayhz5n+ae#65zl)7tfjqdw6@&$#_abzrr2dbefd'
+
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'model',
 ]
 
 MIDDLEWARE = [
