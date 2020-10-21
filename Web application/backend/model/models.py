bb9b76457ba7779576aa9a8d722e76a3165680a9
Juan Manuel Imbachí G <juan.imbachi@correo.icesi.edu.co>
2020-10-20 20:39:30 -0500

    DengueCaseTable modified

--- Web application/backend/model/models.py
+++ Web application/backend/model/models.py
@@ -5,8 +5,10 @@ from django.db import models
 
 # Create your models here.
 class DengueCase(models.Model):
-    city = models.CharField(max_length=50)
-    notification_date = models.DateField()
-    age = models.IntegerField()
-    gender = models.CharField(max_length=10)
-    neighborhood = models.CharField(max_length=50)
+    City = models.CharField(max_length=50)
+    OccurrenceMonth = models.CharField(max_length=20)
+    NotificationDate = models.DateField()
+    Age = models.IntegerField()
+    Gender = models.CharField(max_length=10)
+    Neighborhood = models.CharField(max_length=50)
+    Commune = models.CharField(max_length=10)
