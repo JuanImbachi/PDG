80711ce8f9cceb84cfb5cb4cb401efcaec1a1379
Jamf99 <jorge.morales2@correo.icesi.edu.co>
2020-10-20 23:49:24 -0500

    feat: update data model

--- Web application/backend/model/models.py
+++ Web application/backend/model/models.py
@@ -6,9 +6,8 @@ from django.db import models
 # Create your models here.
 class DengueCase(models.Model):
     City = models.CharField(max_length=50)
-    OccurrenceMonth = models.CharField(max_length=20)
     NotificationDate = models.DateField()
-    Age = models.IntegerField()
+    Age = models.DecimalField(max_digits=4, decimal_places=2)
     Gender = models.CharField(max_length=10)
     Neighborhood = models.CharField(max_length=50)
     Commune = models.CharField(max_length=10)
