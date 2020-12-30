7d3ad2c358b50969ba0ba78fb9cf4e2cad2ac98d
Juan Manuel Imbachí G <juan.imbachi@correo.icesi.edu.co>
2020-12-30 11:55:52 -0500

    Deployment version

--- Web application/backend/backend/settings.py
+++ Web application/backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2w6ipe62hndw$lwx^#*kq&h78*e2^b&cjelh-*%3mr$**a#r@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
